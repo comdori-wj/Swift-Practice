6a86b7fdb43ca22151e2e529670d6db2c158c9b3
COMDORI <02145s1@gmail.com>
2023-10-09 16:31:42 +0900

    Create 오토레이아웃 연습

---
+++ AutoLayoutExamples/AutoLayoutExamplesUITests/AutoLayoutExamplesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AutoLayoutExamplesUITestsLaunchTests.swift
+//  AutoLayoutExamplesUITests
+//
+//  Created by Wonji Ha on 2023/09/21.
+//
+
+import XCTest
+
+final class AutoLayoutExamplesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
