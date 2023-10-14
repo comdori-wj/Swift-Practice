258b2b04b3efd0ab31c8434a404f95f036444ecb
COMDORI <02145s1@gmail.com>
2023-10-14 22:51:05 +0900

    Create 오토레이아웃1 연습

---
+++ PracticeAutoLayout1/PracticeAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeAutoLayout
+//
+//  Created by Janine on 2023/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
