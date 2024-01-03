2b67395c8324f7d2824737e00201040464478b6e
COMDORI <02145s1@gmail.com>
2024-01-03 09:59:22 +0900

    feat SwiftUI-섹션4-스탭7(이미지 넣기)

---
+++ SwiftUI-Essentials/SwiftUI-Essentials/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  SwiftUI-Essentials
+//
+//  Created by Wonji Ha on 1/3/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
