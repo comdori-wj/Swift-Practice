67598d112742fb929b2a80b77e99d6b1d5ef1f9d
comdori-wj <02145s1@gmail.com>
2024-01-31 15:15:57 +0900

    feat 알림의 제목과 내용을 뷰에 표시하는 기능 추가

--- UserNotificationsPractice_Before/UserNotificationsPractice/Infrastructure/UNNotificationReceiver.swift
+++ UserNotificationsPractice_Before/UserNotificationsPractice/Infrastructure/UNNotificationReceiver.swift
@@ -35,27 +35,35 @@ extension UNNotificationReceiver: UNUserNotificationCenterDelegate {
         print("didReceive - UserInfo: \(response.notification.request.content.userInfo)")
         
         if response.notification.request.identifier == "Done" {
-            let keyWindow = UIApplication.shared.connectedScenes
-                .filter({$0.activationState == .foregroundActive})
-                .map({$0 as? UIWindowScene})
-                .compactMap({$0})
-                .first?.windows
-                .filter({$0.isKeyWindow}).first
+//            let keyWindow = UIApplication.shared.connectedScenes
+//                .filter({$0.activationState == .foregroundActive})
+//                .map({$0 as? UIWindowScene})
+//                .compactMap({$0})
+//                .first?.windows
+//                .filter({$0.isKeyWindow}).first
             
 //            let topVC = keyWindow?.rootViewController as? UITabBarController
             //            UIApplication.shared.keyWindow?.rootViewController as? UITabBarController
             //            let innerViewController = InnerViewController()
-            print("sssss")
+
 //            topVC?.selectedIndex = 1
             //                .navigationController?.pushViewController(innerViewController, animated: true)
             
         }
         
+        let title = response.notification.request.content.title
+        let body = response.notification.request.content.body
+        
         let rootView = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController as? UITabBarController
         rootView?.selectedIndex = 1
         let navi = rootView?.selectedViewController as? UINavigationController
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let innerViewController = storyboard.instantiateViewController(identifier: "InnerViewController")
+        guard let innerViewController = storyboard.instantiateViewController(identifier: "InnerViewController") as? InnerViewController else { return }
+        
+        innerViewController.loadViewIfNeeded()
+        innerViewController.notificationTitle.text = title
+        innerViewController.notificationBody.text = body
+        
         navi?.pushViewController(innerViewController, animated: true)
         
         completionHandler()
